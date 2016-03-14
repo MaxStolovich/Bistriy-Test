67560316710b660df7e0ad0d422a6961bcdfc0d2
MaxStolovich <widebonanza@gmail.com>
2016-03-14 11:44:15 +0200

    Context processor is  created

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.conf import global_settings
+
+
+#!!!!!!It is the line I added
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + \
+                              ["django.core.context_processors.request",
+                               "studentsdb.context_processors.students_proc",]
+
+PORTAL_URL = 'http://localhost:8000'
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students',
+#    'django_jenkins',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
